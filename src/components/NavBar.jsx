3e7bf77988315dbddc1985d458405fe2ed923a4e
farisbasha <farisbashatm@gmail.com>
2024-07-27 21:44:50 +0530

    initial

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,24 @@
+
+import { Link } from 'react-router-dom'
+
+export const NavBar = () => {
+  return (
+    <nav className="bg-black border-b border-gray-600 shadow">
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+        <div className="flex items-center justify-between h-16">
+          <div className="flex items-center">
+            <div className="flex-shrink-0">
+              <Link to="/" className="text-white text-lg font-semibold">Home</Link>
+            </div>
+            <div className="hidden md:-my-px md:mr-2 md:ml-4 md:flex md:items-center">
+              <Link to="/jobs" className="ml-4 text-gray-300 hover:text-white">
+                Jobs
+              </Link>
+            </div>
+          </div>
+        </div>
+      </div>
+    </nav>
+  )
+}
+
---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+
+
+const Home = () => {
+  return (
+    <div className="px-10 py-5">
+
+      <h2>Hello there</h2>
+    </div>
+  )
+}
+
+export default Home
